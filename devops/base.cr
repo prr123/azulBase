fbb30f3eff6c1a6cdae9d0415f78c43e252142ca
Peter <priemen@gmail.com>
2026-05-09 15:01:27 +0200

    base files for devops

---
+++ devops/base.cr
@@ -0,0 +1,11 @@
+---
+Account: azul
+Domains:
+- Domain: imgerp.eu
+  Email: admin@imgerp.eu
+  Start: 4/2/2025
+  Country: ES
+  Province: Valencia
+  Locality: Valencia
+  Organisation: azul software
+  OrganisationUnit: admin
